1d4e1a8fcae6f90607bac31dd54938abd2d8e6b0
Abishek Kumar Sah <113754431+AbishekSah@users.noreply.github.com>
2024-11-22 14:34:48 +0530

    Add files via upload

---
+++ Morphie/demo/src/main/java/com/example/springhtop/SpringHtopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhtop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHtopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHtopApplication.class, args);
+	}
+
+}
---
+++ Morphie/demo/src/test/java/com/example/springhtop/SpringHtopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhtop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHtopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
